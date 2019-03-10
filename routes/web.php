5eb2c36f483d6faf930a30fe1c93cb25d1af2fb4
Dastan <koishybaevdb@gmail.com>
2019-03-10 16:46:11 +0500

    Delete registration files

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,39 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'admin'], function() {
+    // Authentication Routes...
+    Route::get('login', [
+        'as' => 'login',
+        'uses' => 'Auth\LoginController@showLoginForm'
+    ]);
+    Route::post('login', [
+        'as' => '',
+        'uses' => 'Auth\LoginController@login'
+    ]);
+    Route::post('logout', [
+        'as' => 'logout',
+        'uses' => 'Auth\LoginController@logout'
+    ]);
+    
+    // Password Reset Routes...
+    Route::post('password/email', [
+        'as' => 'password.email',
+        'uses' => 'Auth\ForgotPasswordController@sendResetLinkEmail'
+    ]);
+    Route::get('password/reset', [
+        'as' => 'password.request',
+        'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm'
+    ]);
+    Route::post('password/reset', [
+        'as' => 'password.update',
+        'uses' => 'Auth\ResetPasswordController@reset'
+    ]);
+    Route::get('password/reset/{token}', [
+        'as' => 'password.reset',
+        'uses' => 'Auth\ResetPasswordController@showResetForm'
+    ]);
+});
