2f1b8fe620000a241c67b3289c341d83fe5e2615
Dastan <koishybaevdb@gmail.com>
2019-03-11 23:39:13 +0500

    Add "show" links and Author "create" link

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-12 col-md-8 mb-3">
+        <div class="col-12 col-md-7 mb-3">
             <ul class="list-group">
                 <li class="list-group-item bg-light font-weight-bold d-flex justify-content-between">
                     <span>Books ({{ $books->count() }})</span>
-                    <a href="{{ route('books.create') }}" class="btn btn-sm btn-dark my-0">
+                    <a href="{{ route('books.create') }}" class="btn btn-sm btn-dark">
                         <i class="fas fa-plus mr-1"></i>
                         Add Book
                     </a>
@@ -16,6 +16,11 @@
                 <li class="list-group-item d-flex justify-content-between">
                     <span>{{ $book->title }}</span>
                     <div class="d-flex">
+                        <div>
+                            <a href="{{ route('books.show', ['id' => $book->id]) }}" class="btn btn-sm btn-dark">
+                                <i class="fas fa-info"></i>
+                            </a>
+                        </div>
                         <div>
                             <a href="{{ route('books.edit', ['id' => $book->id]) }}" class="btn btn-sm btn-dark mx-1">
                                 <i class="fa fa-pen"></i>
@@ -31,13 +36,26 @@
                 @endforeach
             </ul>
         </div>
-        <div class="col-12 col-md-4">
+        <div class="col-12 col-md-5">
             <ul class="list-group">
-                <li class="list-group-item bg-light font-weight-bold">Authors ({{ $authors->count() }})</li>
+                <li class="list-group-item bg-light font-weight-bold d-flex justify-content-between">
+                    <span>Authors ({{ $authors->count() }})</span>
+                    <div>
+                        <a href="{{ route('authors.create') }}" class="btn btn-sm btn-dark">
+                            <i class="fas fa-plus mr-1"></i>
+                            Add Author
+                        </a>
+                    </div>
+                </li>
                 @foreach ($authors as $author)
                 <li class="list-group-item d-flex justify-content-between">
                     <span>{{ $author->name }} {{ $author->surname }} ({{ $author->books->count() }})</span>
                     <div class="d-flex">
+                        <div>
+                            <a href="{{ route('authors.show', ['id' => $author->id]) }}" class="btn btn-sm btn-dark">
+                                <i class="fas fa-info"></i>
+                            </a>
+                        </div>
                         <div>
                             <a href="{{ route('authors.edit', ['id' => $author->id]) }}" class="btn btn-sm btn-dark mx-1">
                                 <i class="fa fa-pen"></i>
